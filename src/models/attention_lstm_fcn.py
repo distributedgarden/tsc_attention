d3ef1ac12b5fda6f06a9d5984b16a411d7ba781b
Adam Walker <oreb@protonmail.com>
2023-11-28 16:29:47 -0700

    (1) update lstmfcn model parameter names; (2) add docstrings and type hints

--- src/models/attention_lstm_fcn.py
+++ src/models/attention_lstm_fcn.py
@@ -4,76 +4,99 @@ import torch.nn.functional as F
 
 from src.models.attention import SelfAttention
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 
 class AttentionLSTMFCN(nn.Module):
     """
-    Description:
-        - Hybrid Long Short-Term Memory Fully Convolutional Network (LSTM-FCN) with Attention
+    A Hybrid Long Short-Term Memory Fully Convolutional Network (LSTM-FCN) with an Attention mechanism.
+    This model enhances the LSTM-FCN by incorporating an attention layer that helps focus on specific parts of the input sequence.
+
+    Attributes:
+        lstm (nn.LSTM): LSTM layer for sequential data processing.
+        attention (SelfAttention): Self-attention layer for focusing on important features.
+        dropout (nn.Dropout): Dropout layer for regularization.
+        conv1, conv2, conv3 (nn.Conv1d): Convolutional layers for feature extraction.
+        bn1, bn2, bn3 (nn.BatchNorm1d): Batch normalization layers corresponding to each convolutional layer.
+        global_avg_pooling (nn.AdaptiveAvgPool1d): Global average pooling layer for feature aggregation.
+        fc (nn.Linear): Fully connected layer for final classification.
+        attention_weights (torch.Tensor): Stores the attention weights computed during the forward pass.
+
+    Args:
+        input_size (int): The number of input features for the LSTM layer.
+        num_classes (int): The number of output classes for classification.
+        hidden_units (int): The number of features in the hidden state of the LSTM.
+        dropout_rate (float): The dropout rate for regularization.
+        cnn_filters (Tuple[int, int, int]): Number of filters for each convolutional layer.
     """
 
-    def __init__(self, max_sequence_length: int, nb_class: int, num_cells: int = 8):
+    def __init__(
+        self,
+        input_size: int,
+        num_classes: int,
+        hidden_units: int = 8,
+        dropout_rate: float = 0.8,
+        cnn_filters: tuple = (128, 256, 128),
+    ):
         super(AttentionLSTMFCN, self).__init__()
 
-        # LSTM layer with self-attention and dropout
-        self.lstm = nn.LSTM(max_sequence_length, num_cells, batch_first=True)
-        self.attention = SelfAttention(num_cells)
-        self.dropout = nn.Dropout(0.8)
+        self.lstm = nn.LSTM(input_size, hidden_units, batch_first=True)
+        self.attention = SelfAttention(hidden_units)
+        self.dropout = nn.Dropout(dropout_rate)
 
-        # CNN layers with batch-normalization
         self.conv1 = nn.Conv1d(
-            1, 128, kernel_size=8, padding="same", padding_mode="zeros"
+            1, cnn_filters[0], kernel_size=8, padding="same", padding_mode="zeros"
         )
-        self.bn1 = nn.BatchNorm1d(128)
+        self.bn1 = nn.BatchNorm1d(cnn_filters[0])
         self.conv2 = nn.Conv1d(
-            128, 256, kernel_size=5, padding="same", padding_mode="zeros"
+            cnn_filters[0],
+            cnn_filters[1],
+            kernel_size=5,
+            padding="same",
+            padding_mode="zeros",
         )
-        self.bn2 = nn.BatchNorm1d(256)
+        self.bn2 = nn.BatchNorm1d(cnn_filters[1])
         self.conv3 = nn.Conv1d(
-            256, 128, kernel_size=3, padding="same", padding_mode="zeros"
+            cnn_filters[1],
+            cnn_filters[2],
+            kernel_size=3,
+            padding="same",
+            padding_mode="zeros",
         )
-        self.bn3 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(cnn_filters[2])
 
-        # global average pooling
         self.global_avg_pooling = nn.AdaptiveAvgPool1d(1)
-
-        # fully connected layer
-        self.fc = nn.Linear(num_cells + 128, nb_class)
+        self.fc = nn.Linear(hidden_units + cnn_filters[2], num_classes)
 
         self.attention_weights = None
 
-    def forward(self, x):
-        # LSTM with dropout forward pass
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass for the AttentionLSTMFCN model.
+
+        Args:
+            x (torch.Tensor): Input tensor of shape (batch_size, sequence_length, input_size).
+
+        Returns:
+            torch.Tensor: Output tensor of shape (batch_size, num_classes).
+        """
         h, _ = self.lstm(x)
         h, attention_weights = self.attention(h)
         h = self.dropout(h[:, -1, :])
 
         self.attention_weights = attention_weights
 
-        # CNN with batch-normalization forward pass
         x_permuted = x.permute(0, 2, 1)
         c = F.relu(self.bn1(self.conv1(x_permuted)))
         c = F.relu(self.bn2(self.conv2(c)))
         c = F.relu(self.bn3(self.conv3(c)))
 
-        # global average pooling
         c = self.global_avg_pooling(c)
         c = c.view(c.size(0), -1)
 
-        # concatenate LSTM and CNN outputs
         combined = torch.cat((h, c), dim=1)
-
-        # fully connected layer with softmax
-        out = F.softmax(self.fc(combined), dim=1)
+        out = self.fc(combined)
 
         return out
-
-
-def example():
-    """
-    Description:
-        - example usage
-    """
-    max_sequence_length = 100
-    nb_class = 5
-    model = AttentionLSTMFCN(max_sequence_length, nb_class)
-    print(model)
--- src/models/lstm_fcn.py
+++ src/models/lstm_fcn.py
@@ -5,78 +5,88 @@ import torch.nn.functional as F
 
 class LSTMFCN(nn.Module):
     """
-    Description:
-        - Hybrid Long Short-Term Memory Fully Convolutional Network (LSTM-FCN)
+    A Hybrid Long Short-Term Memory Fully Convolutional Network (LSTM-FCN) for sequence classification.
+    This model combines LSTM layers for sequential data processing and CNN layers for feature extraction.
+
+    Attributes:
+        lstm (nn.LSTM): LSTM layer for sequential data processing.
+        dropout (nn.Dropout): Dropout layer for regularization.
+        conv1, conv2, conv3 (nn.Conv1d): Convolutional layers for feature extraction.
+        bn1, bn2, bn3 (nn.BatchNorm1d): Batch normalization layers corresponding to each convolutional layer.
+        global_avg_pooling (nn.AdaptiveAvgPool1d): Global average pooling layer for feature aggregation.
+        fc (nn.Linear): Fully connected layer for final classification.
+
+    Args:
+        input_size (int): The number of input features for the LSTM layer.
+        num_classes (int): The number of output classes for classification.
+        hidden_units (int): The number of features in the hidden state of the LSTM.
+        num_lstm_layers (int): The number of recurrent layers in the LSTM.
+        dropout (float): The dropout rate for regularization.
+        cnn_filters (Tuple[int, int, int]): Number of filters for each convolutional layer.
     """
 
-    def __init__(self, MAX_SEQUENCE_LENGTH, NB_CLASS, NUM_CELLS=8):
+    def __init__(
+        self,
+        input_size: int,
+        num_classes: int,
+        hidden_units: int = 8,
+        num_lstm_layers: int = 1,
+        dropout: float = 0.8,
+        cnn_filters: tuple = (128, 256, 128),
+    ):
         super(LSTMFCN, self).__init__()
 
-        # LSTM layer with dropout
-        self.lstm = nn.LSTM(MAX_SEQUENCE_LENGTH, NUM_CELLS, batch_first=True)
-        self.dropout = nn.Dropout(0.8)
+        self.lstm = nn.LSTM(
+            input_size, hidden_units, num_layers=num_lstm_layers, batch_first=True
+        )
+        self.dropout = nn.Dropout(dropout)
 
-        # CNN layers with batch normalization
         self.conv1 = nn.Conv1d(
-            1, 128, kernel_size=8, padding="same", padding_mode="zeros"
+            1, cnn_filters[0], kernel_size=8, padding="same", padding_mode="zeros"
         )
-        self.bn1 = nn.BatchNorm1d(128)
-
+        self.bn1 = nn.BatchNorm1d(cnn_filters[0])
         self.conv2 = nn.Conv1d(
-            128, 256, kernel_size=5, padding="same", padding_mode="zeros"
+            cnn_filters[0],
+            cnn_filters[1],
+            kernel_size=5,
+            padding="same",
+            padding_mode="zeros",
         )
-        self.bn2 = nn.BatchNorm1d(256)
-
+        self.bn2 = nn.BatchNorm1d(cnn_filters[1])
         self.conv3 = nn.Conv1d(
-            256, 128, kernel_size=3, padding="same", padding_mode="zeros"
+            cnn_filters[1],
+            cnn_filters[2],
+            kernel_size=3,
+            padding="same",
+            padding_mode="zeros",
         )
-        self.bn3 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(cnn_filters[2])
 
-        # global average pooling
         self.global_avg_pooling = nn.AdaptiveAvgPool1d(1)
+        self.fc = nn.Linear(hidden_units + cnn_filters[2], num_classes)
 
-        # fully connected layer
-        # LSTM output + CNN output concatenation
-        self.fc = nn.Linear(NUM_CELLS + 128, NB_CLASS)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass for the LSTMFCN model.
 
-    def forward(self, x):
+        Args:
+            x (torch.Tensor): Input tensor of shape (batch_size, sequence_length, input_size).
+
+        Returns:
+            torch.Tensor: Output tensor of shape (batch_size, num_classes).
         """
-        Description:
-            - LSTM forward pass
-            - CNN forward pass
-        """
-        # LSTM with dropout
         h, _ = self.lstm(x)
         h = self.dropout(h[:, -1, :])
 
-        # CNN forward
-        # permute to get the correct input shape for Conv1D
         x_permuted = x.permute(0, 2, 1)
         c = F.relu(self.bn1(self.conv1(x_permuted)))
         c = F.relu(self.bn2(self.conv2(c)))
         c = F.relu(self.bn3(self.conv3(c)))
 
-        # global average pooling
         c = self.global_avg_pooling(c)
-
-        # flatten
         c = c.view(c.size(0), -1)
 
-        # concatenate LSTM and CNN outputs
         combined = torch.cat((h, c), dim=1)
-
-        # Dense layer
-        out = F.softmax(self.fc(combined), dim=1)
+        out = self.fc(combined)
 
         return out
-
-
-def example():
-    """
-    Description:
-        - example usage
-    """
-    max_sequence_length = 100
-    nb_class = 5
-    model = LSTMFCN(max_sequence_length, nb_class)
-    print(model)
