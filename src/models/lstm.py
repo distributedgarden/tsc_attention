418171b58b61f28f679a19650ed4ee833655027b
Adam Walker <oreb@protonmail.com>
2023-11-28 16:32:46 -0700

    (1) update docstrings and add type hints to lstm model

--- src/models/lstm.py
+++ src/models/lstm.py
@@ -1,10 +1,21 @@
+import torch
 import torch.nn as nn
 
 
 class LSTM(nn.Module):
     """
-    Description:
-        - Long Short-Term Memory (LSTM)
+    A basic Long Short-Term Memory (LSTM) network for sequence modeling and classification tasks.
+    This model consists of LSTM layers followed by a fully connected layer for output.
+
+    Attributes:
+        lstm (nn.LSTM): LSTM layers for processing sequential data.
+        fc (nn.Linear): A fully connected (dense) layer for output classification or regression.
+
+    Args:
+        input_size (int): The number of input features in each element of the sequence.
+        hidden_units (int): The number of features in the hidden state of each LSTM layer.
+        num_lstm_layers (int): The number of recurrent layers in the LSTM module.
+        num_classes (int): The number of output classes (for classification tasks) or output features (for regression tasks).
     """
 
     def __init__(
@@ -20,32 +31,18 @@ class LSTM(nn.Module):
         )
         self.fc = nn.Linear(in_features=hidden_units, out_features=num_classes)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
-        Description:
-            - forward pass
+        Forward pass of the LSTM model.
+
+        Args:
+            x (torch.Tensor): The input tensor of shape (batch_size, sequence_length, input_size).
+
+        Returns:
+            torch.Tensor: The output tensor of shape (batch_size, num_classes).
         """
-        # LSTM layers
         out, _ = self.lstm(x)
-
-        # output from the last time step
-        out = out[:, -1, :]
-
-        # fully connected layer
-        out = self.fc(out)
+        out = out[:, -1, :]  # Extract the output from the last time step
+        out = self.fc(out)  # Apply the fully connected layer
 
         return out
-
-
-def example():
-    """
-    Description:
-        - example usage
-    """
-    input_dim = 1
-    hidden_dim = 128
-    num_layers = 2
-    num_classes = 5
-
-    model = LSTM(input_dim, hidden_dim, num_layers, num_classes)
-    print(model)
