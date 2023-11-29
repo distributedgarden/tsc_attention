ea36f15b951d0c90e64e9c11451e950aab1b5c4a
Adam Walker <oreb@protonmail.com>
2023-11-28 18:53:44 -0700

    (1) make cnn models conform to the same implementation detail as the lstm fcn models

--- src/models/attention_os_cnn.py
+++ src/models/attention_os_cnn.py
@@ -7,36 +7,47 @@ from src.models.attention import SelfAttention
 class AttentionOSCNN(nn.Module):
     """
     An Omni-Scale 1 Dimensional Convolutional Neural Network (OS-CNN) with Self-Attention mechanism.
-    This network uses convolutional layers with multiple filter sizes for feature extraction from time-series data,
+    This network uses convolutional layers with specified filter sizes for feature extraction from time-series data,
     combined with a self-attention layer to focus on the most relevant features.
 
     Attributes:
-        conv1_filters (nn.ModuleList): First set of convolutional layers with varying kernel sizes.
-        conv2_filters (nn.ModuleList): Second set of convolutional layers with varying kernel sizes.
-        conv3_filters (nn.ModuleList): Third set of convolutional layers with varying kernel sizes.
+        conv1, conv2, conv3 (nn.Conv1d): Convolutional layers for feature extraction.
+        bn1, bn2, bn3 (nn.BatchNorm1d): Batch normalization layers corresponding to each convolutional layer.
         attention (SelfAttention): Self-attention layer for focusing on relevant features.
         fc (nn.Linear): Fully connected layer for classification.
         attention_weights (torch.Tensor): Stores the attention weights computed during the forward pass.
 
     Args:
         num_classes (int): The number of classes for classification.
+        cnn_filters (Tuple[int, int, int]): Number of filters for each convolutional layer.
     """
 
-    def __init__(self, num_classes: int):
+    def __init__(self, num_classes: int, cnn_filters: tuple = (128, 256, 128)):
         super(AttentionOSCNN, self).__init__()
 
-        self.conv1_filters = nn.ModuleList(
-            [nn.Conv1d(1, 32, kernel_size=ks) for ks in [1, 2, 3, 5, 7, 11]]
+        self.conv1 = nn.Conv1d(
+            1, cnn_filters[0], kernel_size=8, padding="same", padding_mode="zeros"
         )
-        self.conv2_filters = nn.ModuleList(
-            [nn.Conv1d(192, 64, kernel_size=ks) for ks in [1, 2, 3, 5, 7, 11]]
+        self.bn1 = nn.BatchNorm1d(cnn_filters[0])
+        self.conv2 = nn.Conv1d(
+            cnn_filters[0],
+            cnn_filters[1],
+            kernel_size=5,
+            padding="same",
+            padding_mode="zeros",
         )
-        self.conv3_filters = nn.ModuleList(
-            [nn.Conv1d(384, 128, kernel_size=ks) for ks in [1, 2]]
+        self.bn2 = nn.BatchNorm1d(cnn_filters[1])
+        self.conv3 = nn.Conv1d(
+            cnn_filters[1],
+            cnn_filters[2],
+            kernel_size=3,
+            padding="same",
+            padding_mode="zeros",
         )
+        self.bn3 = nn.BatchNorm1d(cnn_filters[2])
 
-        self.attention = SelfAttention(256)
-        self.fc = nn.Linear(256, num_classes)
+        self.attention = SelfAttention(cnn_filters[2])
+        self.fc = nn.Linear(cnn_filters[2], num_classes)
         self.attention_weights = None
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -49,19 +60,11 @@ class AttentionOSCNN(nn.Module):
         Returns:
             torch.Tensor: The output tensor of shape (batch_size, num_classes).
         """
-        conv1_list = [F.relu(conv(x)) for conv in self.conv1_filters]
-        conv1_cat = torch.cat(conv1_list, 1)
-        conv1_bn = F.relu(nn.BatchNorm1d(192)(conv1_cat))
+        c = F.relu(self.bn1(self.conv1(x)))
+        c = F.relu(self.bn2(self.conv2(c)))
+        c = F.relu(self.bn3(self.conv3(c)))
 
-        conv2_list = [F.relu(conv(conv1_bn)) for conv in self.conv2_filters]
-        conv2_cat = torch.cat(conv2_list, 1)
-        conv2_bn = F.relu(nn.BatchNorm1d(384)(conv2_cat))
-
-        conv3_list = [F.relu(conv(conv2_bn)) for conv in self.conv3_filters]
-        conv3_cat = torch.cat(conv3_list, 1)
-        conv3_bn = F.relu(nn.BatchNorm1d(256)(conv3_cat))
-
-        attended, attention_weights = self.attention(conv3_bn.transpose(1, 2))
+        attended, attention_weights = self.attention(c.transpose(1, 2))
         attended_sum = torch.sum(attended, dim=1)
 
         self.attention_weights = attention_weights
--- src/models/os_cnn.py
+++ src/models/os_cnn.py
@@ -5,36 +5,47 @@ import torch.nn.functional as F
 
 class OSCNN(nn.Module):
     """
-    Omni-Scale 1 Dimensional Convolutional Neural Network (OS-CNN), based on the architecture proposed by Tang et al.
-    This model uses convolutional layers with multiple kernel sizes for feature extraction from time-series data,
+    Omni-Scale 1 Dimensional Convolutional Neural Network (OS-CNN).
+    This model uses convolutional layers with specified kernel sizes and filters for feature extraction from time-series data,
     followed by global average pooling and a fully connected layer for classification.
 
     Attributes:
-        conv1_filters (nn.ModuleList): First set of convolutional layers with varying kernel sizes.
-        conv2_filters (nn.ModuleList): Second set of convolutional layers with varying kernel sizes.
-        conv3_filters (nn.ModuleList): Third set of convolutional layers with varying kernel sizes.
+        conv1, conv2, conv3 (nn.Conv1d): Convolutional layers for feature extraction.
+        bn1, bn2, bn3 (nn.BatchNorm1d): Batch normalization layers corresponding to each convolutional layer.
         avg_pool (nn.AdaptiveAvgPool1d): Global average pooling layer.
         fc (nn.Linear): Fully connected layer for classification.
 
     Args:
         num_classes (int): The number of classes for classification.
+        cnn_filters (Tuple[int, int, int]): Number of filters for each convolutional layer.
     """
 
-    def __init__(self, num_classes: int):
+    def __init__(self, num_classes: int, cnn_filters: tuple = (128, 256, 128)):
         super(OSCNN, self).__init__()
 
-        self.conv1_filters = nn.ModuleList(
-            [nn.Conv1d(1, 32, kernel_size=ks) for ks in [1, 2, 3, 5, 7, 11]]
+        self.conv1 = nn.Conv1d(
+            1, cnn_filters[0], kernel_size=8, padding="same", padding_mode="zeros"
         )
-        self.conv2_filters = nn.ModuleList(
-            [nn.Conv1d(192, 64, kernel_size=ks) for ks in [1, 2, 3, 5, 7, 11]]
+        self.bn1 = nn.BatchNorm1d(cnn_filters[0])
+        self.conv2 = nn.Conv1d(
+            cnn_filters[0],
+            cnn_filters[1],
+            kernel_size=5,
+            padding="same",
+            padding_mode="zeros",
         )
-        self.conv3_filters = nn.ModuleList(
-            [nn.Conv1d(384, 128, kernel_size=ks) for ks in [1, 2]]
+        self.bn2 = nn.BatchNorm1d(cnn_filters[1])
+        self.conv3 = nn.Conv1d(
+            cnn_filters[1],
+            cnn_filters[2],
+            kernel_size=3,
+            padding="same",
+            padding_mode="zeros",
         )
+        self.bn3 = nn.BatchNorm1d(cnn_filters[2])
 
         self.avg_pool = nn.AdaptiveAvgPool1d(1)
-        self.fc = nn.Linear(256, num_classes)
+        self.fc = nn.Linear(cnn_filters[2], num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -46,19 +57,11 @@ class OSCNN(nn.Module):
         Returns:
             torch.Tensor: The output tensor of shape (batch_size, num_classes).
         """
-        conv1_list = [F.relu(conv(x)) for conv in self.conv1_filters]
-        conv1_cat = torch.cat(conv1_list, 1)
-        conv1_bn = F.relu(nn.BatchNorm1d(192)(conv1_cat))
+        c = F.relu(self.bn1(self.conv1(x)))
+        c = F.relu(self.bn2(self.conv2(c)))
+        c = F.relu(self.bn3(self.conv3(c)))
 
-        conv2_list = [F.relu(conv(conv1_bn)) for conv in self.conv2_filters]
-        conv2_cat = torch.cat(conv2_list, 1)
-        conv2_bn = F.relu(nn.BatchNorm1d(384)(conv2_cat))
-
-        conv3_list = [F.relu(conv(conv2_bn)) for conv in self.conv3_filters]
-        conv3_cat = torch.cat(conv3_list, 1)
-        conv3_bn = F.relu(nn.BatchNorm1d(256)(conv3_cat))
-
-        pooled = self.avg_pool(conv3_bn)
+        pooled = self.avg_pool(c)
         pooled_flat = pooled.view(pooled.size(0), -1)
 
         output = self.fc(pooled_flat)
